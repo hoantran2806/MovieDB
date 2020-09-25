00e9c316c761428c27572376b6b7ff9c6dc93985
HoanNT14 <v.hoannt14@vinsmart.net.vn>
2020-09-25 10:33:26 +0700

    implement Search function using debounce operator (not complete)

--- app/src/main/java/hoannt/android/moviedb/ui/search/SearchFragment.kt
+++ app/src/main/java/hoannt/android/moviedb/ui/search/SearchFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -19,8 +20,12 @@ import hoannt.android.moviedb.ui.base.customview.PagerSnapHelper
 import hoannt.android.moviedb.ui.list.adapter.MovieListAdapter
 import hoannt.android.moviedb.ui.list.viewmodel.ShareMovieViewModel
 import hoannt.android.moviedb.ui.search.viewmodel.SearchListViewModel
+import io.reactivex.Observable
+import io.reactivex.functions.Predicate
+import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.fragment_search.*
 import kotlinx.android.synthetic.main.fragment_search.view.*
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 
@@ -55,6 +60,8 @@ class SearchFragment : Fragment(), MovieListAdapter.RecyclerViewItemClick {
                 list_searched.adapter = searchListAdapter
             }
         })
+
+
         return view
     }
 
@@ -63,28 +70,44 @@ class SearchFragment : Fragment(), MovieListAdapter.RecyclerViewItemClick {
             LinearLayoutManager(requireActivity(), RecyclerView.HORIZONTAL, false)
         var snapHelper = PagerSnapHelper()
         snapHelper.attachToRecyclerView(view.list_searched)
-//        view.edt_search.addTextChangedListener(object :TextWatcher{
-//            override fun afterTextChanged(s: Editable?) {
-//                if (!s.isNullOrEmpty()){
-//                    searchListViewModel.search(s.toString(), 1L)
-//                }
-//
-//            }
-//
-//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-//
-//            }
-//
-//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//
-//            }
-//
-//        })
         view.btn_search.setOnClickListener {
-            if (!view.edt_search.text.isNullOrEmpty()) {
-                searchListViewModel.search(view.edt_search.text.toString(), 1L)
+            if (!view.edt_search.query.isNullOrEmpty()) {
+                searchListViewModel.search(view.edt_search.query.toString(), 1L)
             }
         }
+        searchFromView(view.edt_search).debounce(300, TimeUnit.MILLISECONDS)
+            .filter(object : Predicate<String> {
+                override fun test(t: String): Boolean {
+                    return !t.isNullOrEmpty()
+                }
+            })
+            .distinctUntilChanged()
+    }
+
+    private fun searchFromView(searchView: SearchView): Observable<String> {
+        var subject: PublishSubject<String> = PublishSubject.create()
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                subject.onComplete()
+                Log.i(TAG, "onQueryTextSubmit: $query")
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != null) {
+                    subject.onNext(newText)
+//                    Log.i(TAG, "onQueryTextChange: newText = $newText")
+//                    subject
+//                        .debounce(500, TimeUnit.MILLISECONDS).subscribe(Consumer {
+////                            searchListViewModel.search(it, 1L)
+//                            Log.i(TAG, "onQueryTextChange using debounce: $it")
+//                        })
+                }
+                return true
+            }
+
+        })
+        return subject
     }
 
     override fun onItemSelected(movieEntity: MovieEntity, imageView: ImageView) {
